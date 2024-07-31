209ee0470d835064ac5b6752fc2aabbc2e103594
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-07-31 23:32:22 +0900

    Feat: 박스 컴포넌트 제작

---
+++ service/storybook/stories/ReactComponents/Layout/Box.stories.tsx
@@ -0,0 +1,20 @@
+import {Box as _Box} from "@byeonghyeon/react-components-layout"
+import "@byeonghyeon/react-components-layout/style.css"
+
+export default {
+  title: "React Components/Layout/Box",
+  component: _Box,
+  parameters:{
+    layout: "centered"
+  },
+  tags: ["autodocs"]
+}
+
+export const BlockStory = {
+  args: {
+    padding: "5",
+    background: "pink",
+    boxShadow: "xl",
+    borderRadius: "md"
+  }
+};
