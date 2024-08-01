50671ee33ad0791119dfd79f0e53374fc94ab824
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-01 23:14:45 +0900

    Feat: Divider 컴포넌트 생성

---
+++ package/react/components/layout/src/layout/Divider.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { DividerProps } from "./types";
+import { vars } from "@byeonghyeon/themes";
+
+function Divider(props: DividerProps, ref: React.Ref<HTMLHRElement>) {
+  const {
+    color = "gray",
+    variant = "solid",
+    size = 1,
+    orientaion = "horizontal"
+  } = props;
+
+  const borderStyle =
+    orientaion === "horizontal"
+      ? {
+          width: "100%",
+          borderWidth: `0 0 ${size}px 0`,
+        }
+      : {
+          height: "100%",
+          borderWidth: `0 0 0 ${size}px`,
+        };
+
+  return (
+    <hr
+      {...props}
+      ref={ref}
+      style={{
+        borderStyle: variant,
+        borderColor: color && vars.colors.$scale?.[color]?.[200],
+        ...borderStyle,
+        ...props.style,
+      }}
+    />
+  );
+}
+
+const _Diver = React.forwardRef(Divider);
+export {_Diver as Divider}
