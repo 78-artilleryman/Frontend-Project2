c43777f05595317de5eb5b344d0a19a584dcdabf
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-04 23:50:19 +0900

    Feat: 아코디언 컴포넌트 생성

---
+++ service/storybook/stories/ReactComponents/Accordion/Accordion.stories.tsx
@@ -0,0 +1,47 @@
+import "@byeonghyeon/react-components-accordion/style.css";
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel } from "@byeonghyeon/react-components-accordion";
+import React from "react";
+
+import "@byeonghyeon/react-components-layout/style.css";
+import { Text, Heading } from "@byeonghyeon/react-components-layout";
+
+export default {
+  title: "React Components/Accordion",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const AccordionStory = {
+  render: () => (
+    <Accordion defaultActiveItems={["목록1"]} style={{ width: "500px" }}>
+      <AccordionItem itemName="목록1">
+        <AccordionButton>
+          <Heading color="gray" fontSize="lg">
+            목록 1
+          </Heading>
+        </AccordionButton>
+        <AccordionPanel>
+          <Text color="gray" fontSize="md">
+            내용입니다.
+          </Text>
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem itemName="목록2">
+        <AccordionButton>
+          <Heading color="gray" fontSize="lg">
+            목록 2
+          </Heading>
+        </AccordionButton>
+        <AccordionPanel>
+          <Text color="gray" fontSize="md">
+            내용입니다.
+            <br />
+            내용입니다.
+          </Text>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  ),
+};
