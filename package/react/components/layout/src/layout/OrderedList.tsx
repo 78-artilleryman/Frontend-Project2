d543a8d9284c02c2a7414636f7dec3c0a5fe1d8d
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-05 00:15:31 +0900

    Feat: 리스트 컴포넌트 생성

---
+++ package/react/components/layout/src/layout/OrderedList.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { OrderListProps } from "./types";
+import { vars } from "@byeonghyeon/themes";
+import { Flex } from "./Flex";
+
+function OrderedList(props: OrderListProps, ref: React.Ref<HTMLOListElement>) {
+  const { spacing = 3, children, ...rest } = props;
+
+  return (
+    <Flex
+      {...rest}
+      as="ol"
+      ref={ref}
+      direction="column"
+      style={{ gap: vars.box.spacing[spacing], listStyleType: "decimal" }}>
+      {children}
+    </Flex>
+  );
+}
+
+const _OrderedList = React.forwardRef(OrderedList);
+export { _OrderedList as OrderedList };
