a3970571248c63b1f3658f3ff8780a4748acd2c1
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-06 17:46:42 +0900

    Feat: Toast 컴포넌트 생성

---
+++ service/storybook/stories/ReactComponents/Toast/Toast.stories.tsx
@@ -0,0 +1,38 @@
+import "@byeonghyeon/react-components-button/style.css";
+import { Button } from "@byeonghyeon/react-components-button";
+import React from "react";
+
+import { ToastProvider, useToast } from "@byeonghyeon/react-components-toast";
+import "@byeonghyeon/react-components-toast/style.css";
+
+export default {
+  title: "React Components/Toast",
+  parameters: {
+    layout: "centered",
+  },
+};
+
+const Example = () => {
+  const { toast } = useToast();
+
+  return (
+    <Button
+      onClick={() =>
+        toast({
+          payload: {
+            message: "Toast 메시지",
+          },
+        })
+      }>
+      Toast Button
+    </Button>
+  );
+};
+
+export const ToastStory = {
+  render: () => (
+    <ToastProvider>
+      <Example />
+    </ToastProvider>
+  ),
+};
