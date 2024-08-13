034dd300debcb6f25947ef809fb9fb95983e1dba
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-13 21:40:37 +0900

    Feat: supabase 연결

---
+++ service/pluma/src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "./util/supabase/middleware";
+
+export async function middleware(request: NextRequest) {
+  const { supabase, response } = createClient(request);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user?.role !== "authenticated") return NextResponse.redirect(new URL("/admin", request.url));
+  return response;
+}
+
+export const config = {
+  matcher: "/write",
+};
