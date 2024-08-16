f9de9a9f955ed6333d5e747c0305dbb18db5241a
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-16 23:56:23 +0900

    Design: 로그인 페이지 생성 및 디자인

---
+++ service/pluma/src/app/login/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+import { Box } from "@byeonghyeon/react-components-layout";
+import { Input } from "@byeonghyeon/react-components-input";
+import { Button } from "@byeonghyeon/react-components-button";
+
+function Loginpage() {
+  return (
+    <div className="w-screen h-screen bg-custom-Page-gradient flex items-center justify-center">
+      <Box className="bg-color-white w-[1000px] flex items-center rounded-[20px] h-[645px]">
+        <div className="w-[50%] bg-custom-box-gradient h-full flex flex-col pl-16 rounded-[20px] rounded-r-none pt-[176px]">
+          <h1 className="text-7xl font-bold text-blue-900 mb-3">Pluma</h1>
+          <h2 className="text-6xl font-bold text-blue-700">Login Page</h2>
+        </div>
+        <div className="w-[50%] flex flex-col items-center gap-11">
+          <h3 className="text-4xl font-bold text-blackAlpha-900">User Login</h3>
+          <form className="w-[50%] flex flex-col items-center gap-7">
+            <Input type="text" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <Button color="blue">Login</Button>
+          </form>
+          <p className="text-lg font-normal text-gray-400">Create Your Account</p>
+        </div>
+      </Box>
+    </div>
+  );
+}
+
+export default Loginpage;
--- service/pluma/tailwind.config.ts
+++ service/pluma/tailwind.config.ts
@@ -1,3 +1,4 @@
+import { vars } from "@byeonghyeon/themes";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -10,10 +11,12 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-Page-gradient": "linear-gradient(0deg, #2B6CB0 0%, #2C5282 35.5%, #1A365D 93.5%)",
+        "custom-box-gradient": "linear-gradient(180deg, #F8F8F8 25%, #BCDDF6 59%)",
       },
     },
+    colors: vars.colors.$static.light,
   },
   plugins: [],
 };
