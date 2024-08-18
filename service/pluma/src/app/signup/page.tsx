e7a9d5810499ac36842f306688ce6bc8bace0d02
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-18 21:46:09 +0900

    Design: 회원가입 페이지 생성 및 디자인

--- service/pluma/src/app/signup/page.tsx
+++ service/pluma/src/app/signup/page.tsx
@@ -6,27 +6,28 @@ import { Box } from "@byeonghyeon/react-components-layout";
 import Link from "next/link";
 import React from "react";
 
-function LoginPage() {
+function SignupPage() {
   return (
     <div className="w-screen h-screen bg-custom-Page-gradient flex items-center justify-center">
       <Box className="bg-color-white w-[1000px] flex items-center rounded-[20px] h-[645px]">
         <div className="w-[50%] bg-custom-box-gradient h-full flex flex-col pl-16 rounded-[20px] rounded-r-none pt-[176px]">
           <h1 className="text-7xl font-bold text-blue-900 mb-3">Pluma</h1>
-          <h2 className="text-6xl font-bold text-blue-700">Login Page</h2>
+          <h2 className="text-6xl font-bold text-blue-700">SignUp Page</h2>
         </div>
         <div className="w-[50%] flex flex-col items-center gap-11">
           <h3 className="text-4xl font-bold text-blackAlpha-900">
-            User <span className="text-blue-600">Login</span>
+            Create <span className="text-blue-600">Account</span>
           </h3>
           <form className="w-[50%] flex flex-col items-center gap-7">
             <Input type="text" placeholder="Email" />
             <Input type="password" placeholder="Password" />
+            <Input type="text" placeholder="name" />
             <Button size="md" color="blue" className="w-[250px] flex justify-center rounded-[20px]">
-              Login
+              SIGN UP
             </Button>
           </form>
-          <Link href="/signup" className="text-lg font-normal text-gray-400">
-            Create Your Account
+          <Link href="/login" className="text-lg font-normal text-gray-400">
+            SIGN IN
           </Link>
         </div>
       </Box>
@@ -34,4 +35,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default SignupPage;
