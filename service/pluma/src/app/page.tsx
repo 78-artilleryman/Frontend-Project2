6eaeec3c5f13d1777c5ce73c7711d1083c5eef27
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-19 19:19:30 +0900

    Remove: 불필요한 코드 제거

--- service/pluma/src/app/page.tsx
+++ service/pluma/src/app/page.tsx
@@ -1,11 +1,4 @@
-import { cookies } from "next/headers";
-import { serverCreateClient } from "@/util/supabase/server";
-
 export default function Home() {
-  const cookieStore = cookies();
-  const supabase = serverCreateClient(cookieStore);
-  console.log(supabase);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Home 페이지</p>
--- service/pluma/src/components/FormField.tsx
+++ service/pluma/src/components/FormField.tsx
@@ -3,11 +3,10 @@
 import { Button } from "@byeonghyeon/react-components-button";
 import { Input } from "@byeonghyeon/react-components-input";
 import React from "react";
-import { handleSubmit } from "@/app/signup/signupAction";
 
 function FormField() {
   return (
-    <form action={handleSubmit} className="w-[50%] flex flex-col items-center gap-7">
+    <form className="w-[50%] flex flex-col items-center gap-7">
       <Input type="email" placeholder="Email" name="email" />
       <Input type="password" placeholder="Password" name="password" />
       <Input type="text" placeholder="name" name="name" />
