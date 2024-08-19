356c1a8a8854f94077082ee2a14f66eab7d846a1
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-19 20:23:21 +0900

    Refactor: next auth route 로직 개선

--- service/pluma/src/app/api/[...nextauth]/route.ts
+++ service/pluma/src/app/api/[...nextauth]/route.ts
@@ -1,36 +1,52 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
-import Google from "next-auth/providers/google";
-import Kakao from "next-auth/providers/kakao";
-import Naver from "next-auth/providers/naver";
+import GoogleProvider from "next-auth/providers/google";
+import KakaoProvider from "next-auth/providers/kakao";
+import NaverProvider from "next-auth/providers/naver";
 import prisma from "@/db";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
   session: {
     strategy: "jwt" as const,
     maxAge: 60 * 60 * 24,
     updateAge: 60 * 60 * 2,
   },
-
   providers: [
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
-    Naver({
+    NaverProvider({
       clientId: process.env.NAVER_CLIENT_ID || "",
       clientSecret: process.env.NAVER_CLIENT_SECRET || "",
     }),
-    Kakao({
+    KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID || "",
       clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
     }),
   ],
   pages: {
-    signIn: "/login",
+    signIn: "/signin",
+  },
+  callbacks: {
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.sub,
+      },
+    }),
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
   },
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
