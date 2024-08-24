542ff57551db6b9525acbf44ca266ddb22cc4a84
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-24 23:40:42 +0900

    Feat: novel container 컴포넌트 react-query 연결

--- service/pluma/src/app/novel/components/NoverListContainer.tsx
+++ service/pluma/src/app/novel/components/NoverListContainer.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import React from "react";
+import { useFetchNovelContainerQuery } from "../queries/novelQuery";
 import NovelBox from "./NovelBox";
 
 function NoverListContainer() {
+  const { data: novelContainerList } = useFetchNovelContainerQuery({ sort: "createdAt", page: 1, limit: 8 });
   return (
     <ul className="grid grid-cols-5 gap-10 tbr:grid-cols-4 tbr:gap-7 tbc:grid-cols-4 tbc:gap-7 mb:grid-cols-3 mb:gap-4">
-      <NovelBox />
+      {novelContainerList && novelContainerList.pages[0]?.novels.map(novel => <NovelBox key={novel.id} />)}
     </ul>
   );
 }
