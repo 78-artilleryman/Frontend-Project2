f768713ed97357ec01b8548850aed72751d8bbf0
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-24 20:57:08 +0900

    Refactor: 메인페이지 서비스 로직 분리

--- service/pluma/src/app/novel/page.tsx
+++ service/pluma/src/app/novel/page.tsx
@@ -1,39 +1,9 @@
 import React from "react";
-import { getCookieData } from "../util/getCookie";
 import Banner from "./components/Banner";
 import NovelListSlider from "./components/NovelListSlider";
 import NoverListContainer from "./components/NoverListContainer";
 import SlideControlButton from "./components/SlideControlButton";
-
-async function fetchNovels({ sort = "updatedAt", page = 1, limit = 8 }) {
-  const token = await getCookieData("next-auth.session-token");
-
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/novels?sort=${sort}&page=${page}&limit=${limit}`,
-      {
-        cache: "no-store",
-        method: "GET",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + token?.value,
-        },
-      }
-    );
-
-    if (!response.ok) {
-      console.log("Response status:", response.status);
-      console.log("Response body:", await response.text());
-      throw new Error("Network response was not ok");
-    }
-
-    const novelListData = await response.json();
-    return novelListData;
-  } catch (error) {
-    console.error("Fetch error:", error);
-  }
-}
+import { fetchNovels } from "./services/novelService";
 
 async function NovelPage() {
   const data = await fetchNovels({});
---
+++ service/pluma/src/app/novel/services/novelService.ts
@@ -0,0 +1,31 @@
+import { getCookieData } from "@/app/util/getCookie";
+
+export async function fetchNovels({ sort = "updatedAt", page = 1, limit = 8 }) {
+  const token = await getCookieData("next-auth.session-token");
+
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/novels?sort=${sort}&page=${page}&limit=${limit}`,
+      {
+        cache: "no-store",
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token?.value,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      console.log("Response status:", response.status);
+      console.log("Response body:", await response.text());
+      throw new Error("Network response was not ok");
+    }
+
+    const novelListData = await response.json();
+    return novelListData;
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+}
