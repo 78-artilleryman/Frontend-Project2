ff3a3c803dbfb20403c5f4edda358d761ab260b5
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-29 23:16:34 +0900

    Feat: Novel 생성하는 박스 컴포넌트 추가

---
+++ service/pluma/src/app/novel/components/CreateNovelBox.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { IoIosAddCircleOutline } from "react-icons/io";
+
+function CreateNovelBox() {
+  return (
+    <li className="relative w-[230px] h-[280px] bg-[#d9d9d9] rounded-[20px] tbr:w-[200px] tbr:h-[270px] tbc:w-[140px] tbc:h-[210px] tbc:rounded-xl mb:w-[90px] mb:h-[140px] cursor-pointer group aspect-square flex justify-center items-center">
+      <IoIosAddCircleOutline size={60} color="gray" />
+    </li>
+  );
+}
+
+export default CreateNovelBox;
--- service/pluma/src/app/novel/components/NoverListContainer.tsx
+++ service/pluma/src/app/novel/components/NoverListContainer.tsx
@@ -2,12 +2,14 @@
 
 import React from "react";
 import { useFetchNovelContainerQuery } from "../queries/novelQuery";
+import CreateNovelBox from "./CreateNovelBox";
 import NovelBox from "./NovelBox";
 
 function NoverListContainer() {
   const { data: novelContainerList } = useFetchNovelContainerQuery({ sort: "createdAt", page: 1, limit: 8 });
   return (
     <ul className="grid grid-cols-5 gap-10 tbr:grid-cols-4 tbr:gap-7 tbc:grid-cols-4 tbc:gap-7 mb:grid-cols-3 mb:gap-4">
+      <CreateNovelBox />
       {novelContainerList && novelContainerList.pages[0]?.novels.map(novel => <NovelBox key={novel.id} {...novel} />)}
     </ul>
   );
