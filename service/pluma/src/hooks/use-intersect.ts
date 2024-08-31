b0692591f14eb11b10359b6478a32e2ce9fe5736
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-08-31 14:27:33 +0900

    Refactor: use-intersect hook 수정

---
+++ service/pluma/src/hooks/use-intersect.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useRef } from "react";
+
+// eslint-disable-next-line no-unused-vars
+type Intersect = (entry: IntersectionObserverEntry, observer: IntersectionObserver) => void;
+
+export default function useIntersect<T extends HTMLElement>(onIntersect: Intersect) {
+  const ref = useRef<T>(null);
+
+  const callback = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) onIntersect(entry, observer);
+      });
+    },
+    [onIntersect]
+  );
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const observer = new IntersectionObserver(callback);
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, [callback]);
+
+  return ref;
+}
