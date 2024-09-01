3326c1f2d2719d491869e70e8cfacad9ca980caf
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-09-01 22:38:57 +0900

    Feat: novel 생성 페이지 구현

---
+++ service/pluma/src/app/novel/create/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NovelCreatePage() {
+  return (
+    <div className="w-screen h-screen bg-custom-Page-gradient flex items-center justify-center">
+      <div className="bg-color-white w-[1000px] flex items-center rounded-[20px] h-[645px]">
+        <div className="w-full bg-custom-box-gradient h-full flex flex-col pl-16 rounded-[20px] pt-[176px]" />
+      </div>
+    </div>
+  );
+}
+
+export default NovelCreatePage;
