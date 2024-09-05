83c1b7597d70c1e8ac6a89fe51ae42542f36fe0b
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-09-05 21:19:25 +0900

    Fix: 로그인 소셜 아이콘 위치 수정

--- service/pluma/src/components/signin/FormField.tsx
+++ service/pluma/src/components/signin/FormField.tsx
@@ -4,9 +4,6 @@ import { Button } from "@byeonghyeon/react-components-button";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
 import React from "react";
-import googleIcon from "../../../../public/signIn-Icon/google_logo.svg";
-import kakaoIcon from "../../../../public/signIn-Icon/kakao_logo.svg";
-import naverIcon from "../../../../public/signIn-Icon/naver_logo.svg";
 
 function FormField() {
   const handleClickGoogle = (type: string) => {
@@ -18,27 +15,27 @@ function FormField() {
   };
 
   return (
-    <form className="w-[50%] flex flex-col items-center gap-7">
+    <form className="flex w-[50%] flex-col items-center gap-7">
       <Button
         size="md"
-        className="w-[250px] bg-[#f2f2f2] text-gray-600 pl-[80px] hover:text-whiteAlpha-900"
-        leftIcon={<Image src={googleIcon} alt="구글 로그인" width={15} height={15} priority />}
+        className="hover:text-whiteAlpha-900 w-[250px] bg-[#f2f2f2] pl-[80px] text-gray-600"
+        leftIcon={<Image src="/signIn-Icon/google_logo.svg" alt="구글 로그인" width={15} height={15} priority />}
         onClick={() => handleClickGoogle("google")}>
         Google
       </Button>
       <Button
         size="md"
         color="yellow"
-        className="w-[250px] bg-[#FDDC3F] text-blackAlpha-900 pl-[80px]"
-        leftIcon={<Image src={kakaoIcon} alt="구글 로그인" width={20} height={20} priority />}
+        className="text-blackAlpha-900 w-[250px] bg-[#FDDC3F] pl-[80px]"
+        leftIcon={<Image src="/signIn-Icon/kakao_logo.svg" alt="구글 로그인" width={20} height={20} priority />}
         onClick={() => handleClickGoogle("kakao")}>
         Kakao
       </Button>
       <Button
         size="md"
         color="green"
-        className="w-[250px] bg-[#36AB36] text-whiteAlpha-900 pl-[85px]"
-        leftIcon={<Image src={naverIcon} alt="구글 로그인" width={15} height={15} priority />}
+        className="text-whiteAlpha-900 w-[250px] bg-[#36AB36] pl-[85px]"
+        leftIcon={<Image src="/signIn-Icon/naver_logo.svg" alt="구글 로그인" width={15} height={15} priority />}
         onClick={() => handleClickGoogle("naver")}>
         Naver
       </Button>
