1b312644f8b83dad4022759a9d0b8ff898402522
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-09-12 22:15:32 +0900

    Feat: 소설 생성 페이지로 가는 Link 연결

--- service/pluma/src/components/novel/Novel-Create-Box.tsx
+++ service/pluma/src/components/novel/Novel-Create-Box.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import React from "react";
 import { IoIosAddCircleOutline } from "react-icons/io";
 
 function CreateNovelBox() {
   return (
-    <li className="relative w-[230px] h-[280px] bg-[#d9d9d9] rounded-[20px] tbr:w-[200px] tbr:h-[270px] tbc:w-[140px] tbc:h-[210px] tbc:rounded-xl mb:w-[90px] mb:h-[140px] cursor-pointer group aspect-square flex justify-center items-center hover:bg-[rgba(0,0,0,0.3)]">
-      <IoIosAddCircleOutline size={60} color="gray" />
-    </li>
+    <Link href="/novel/create">
+      <li className="tbr:w-[200px] tbr:h-[270px] tbc:w-[140px] tbc:h-[210px] tbc:rounded-xl mb:w-[90px] mb:h-[140px] group relative flex aspect-square h-[280px] w-[230px] cursor-pointer items-center justify-center rounded-[20px] bg-[#d9d9d9] hover:bg-[rgba(0,0,0,0.3)]">
+        <IoIosAddCircleOutline size={60} color="gray" />
+      </li>
+    </Link>
   );
 }
 
