a1cbcb09eb081ab860b09e77c6566764db5717ed
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-09-19 22:53:13 +0900

    Feat: novel 생성 때 사용할 상태 생성

--- service/pluma/src/store/novel/novelStore.ts
+++ service/pluma/src/store/novel/novelStore.ts
@@ -8,9 +8,60 @@ type NovelState = {
   clearNovel: () => void;
 };
 
+type CreateNovelState = {
+  novel: NovelType;
+  // eslint-disable-next-line no-unused-vars
+  updateField: <K extends keyof NovelType>(field: K, value: NovelType[K]) => void;
+  resetNovel: () => void;
+};
+
 // 상태 생성
 export const useNovelStore = create<NovelState>(set => ({
   novel: null,
   setNovel: novel => set({ novel }),
   clearNovel: () => set({ novel: null }),
 }));
+
+// novel 생성값 상태
+export const createNovelStore = create<CreateNovelState>(set => ({
+  novel: {
+    title: "",
+    description: "",
+    image: null,
+    userId: "",
+    genres: [],
+    created_at: "",
+    updated_at: "",
+  },
+
+  // 하나의 함수로 모든 필드를 업데이트
+  updateField: <K extends keyof NovelType>(field: K, value: NovelType[K]) =>
+    set(state => ({
+      novel: {
+        ...state.novel,
+        [field]: value,
+      },
+    })),
+
+  // Novel 상태 리셋 함수
+  resetNovel: () =>
+    set(() => ({
+      novel: {
+        title: "",
+        description: "",
+        image: null,
+        userId: "",
+        genres: [],
+        created_at: "",
+        updated_at: "",
+      },
+    })),
+}));
+
+/*
+const { updateField, resetNovel } = createNovelStore();
+updateField('title', '새로운 제목');
+updateField('description', '설명 업데이트');
+updateField('genres', ['fantasy', 'adventure']);
+resetNovel();
+*/
--- service/pluma/src/types/novel/novel.type.ts
+++ service/pluma/src/types/novel/novel.type.ts
@@ -1,10 +1,11 @@
 import { PropsWithChildren } from "react";
 
 export interface NovelType extends PropsWithChildren {
-  id: string;
+  id?: string;
   title: string;
   description: string;
   userId: string;
+  image?: string | null;
   created_at: string;
   updated_at: string;
   genres: {
