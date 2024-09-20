c172cacf5611ba420ac45fb6381892a1f969efd3
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-09-20 23:47:00 +0900

    Feat: 전역 상태 input 컴포넌트에 연결

--- service/pluma/src/components/create/Create-Input.tsx
+++ service/pluma/src/components/create/Create-Input.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import { Input } from "@byeonghyeon/react-components-input";
-import React from "react";
+import React, { ChangeEvent } from "react";
+import { createNovelStore } from "@/store/novel/novelStore";
 
 function CreateInput() {
+  const { novel, updateField } = createNovelStore();
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    updateField(name as keyof typeof novel, value);
+  };
+
   return (
     <div className="flex w-[45%] flex-col gap-2">
       <label htmlFor="title" className="text-blackAlpha-900 text-base font-bold">
         제목
       </label>
-      <Input id="title" className="bg-whiteAlpha-900 focus:bg-whiteAlpha-900 h-10" />
+
+      <Input
+        id="title"
+        name="title" // name 속성을 필수로 추가
+        className="bg-whiteAlpha-900 focus:bg-whiteAlpha-900 h-10"
+        value={novel.title}
+        onChange={onChange}
+      />
     </div>
   );
 }
