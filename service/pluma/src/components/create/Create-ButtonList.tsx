194d1ac11dd2751da068172dcb767dabb4ec66bf
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-09-22 21:48:59 +0900

    Feat: novel 생성 후 페이지 이동 로직 추가

--- service/pluma/src/components/create/Create-ButtonList.tsx
+++ service/pluma/src/components/create/Create-ButtonList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@byeonghyeon/react-components-button";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { useNovelMutation } from "@/queries/novel/nevelMutation";
 import { createNovelStore } from "@/store/novel/novelStore";
@@ -8,8 +9,12 @@ import { createNovelStore } from "@/store/novel/novelStore";
 function CreateButtonList() {
   const { novel } = createNovelStore();
   const { mutate } = useNovelMutation("", "post", novel);
+
+  const router = useRouter();
+
   const handlePostNovel = async () => {
     mutate();
+    router.push("/novel");
   };
 
   return (
