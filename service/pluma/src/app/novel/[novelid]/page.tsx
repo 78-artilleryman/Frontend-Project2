d12728f1277156daa58d918d293c404aaa5b7caa
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-10-15 23:15:45 +0900

    Feat: side nav bar 생성

--- service/pluma/src/app/novel/[novelid]/page.tsx
+++ service/pluma/src/app/novel/[novelid]/page.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import NavBar from "@/components/edit/NavBar";
 import QuillEditor from "@/components/edit/QuillEditor";
 
 function NovelDetailPage() {
   return (
     <main>
-      <div className="bg-blackAlpha-300 h-10 w-full">
+      <header className="bg-blackAlpha-300 h-10 w-full">
         <div />
-      </div>
+      </header>
       <div className="flex h-screen w-full">
-        <div className="bg-blackAlpha-300 h-full w-20" />
+        <NavBar />
         <div className="bg-blackAlpha-900 h-full w-[200px]" />
         <div className="mx-auto w-[60%]">
           <QuillEditor />
---
+++ service/pluma/src/components/edit/NavBar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+import { LuFileEdit } from "react-icons/lu";
+import { LuSettings } from "react-icons/lu";
+import { MdOutlinePeopleAlt } from "react-icons/md";
+import { RiTimerLine } from "react-icons/ri";
+import { TbWorldPlus } from "react-icons/tb";
+
+function NavBar() {
+  const urlSearch = useSearchParams();
+  const novelId = urlSearch.get("novelid");
+
+  return (
+    <section className="bg-whiteAlpha-900 relative flex h-full w-[60px] flex-col items-center gap-2 p-2">
+      {/* 소설 리스트 페이지 이동 */}
+      <Link href={`novel/${novelId}/`}>
+        <button
+          className="hover:bg-blackAlpha-300 flex h-12 w-12 items-center justify-center rounded-lg px-2"
+          type="button">
+          <LuFileEdit size={22} />
+        </button>
+      </Link>
+
+      {/* 인물 설정 페이지 이동 */}
+      <Link href={`novel/${novelId}/`}>
+        <button
+          className="hover:bg-blackAlpha-300 flex h-12 w-12 items-center justify-center rounded-lg px-2"
+          type="button">
+          <MdOutlinePeopleAlt size={22} />
+        </button>
+      </Link>
+
+      {/* 세계관 설정 페이지 이동 */}
+      <Link href={`novel/${novelId}/`}>
+        <button
+          className="hover:bg-blackAlpha-300 flex h-12 w-12 items-center justify-center rounded-lg px-2"
+          type="button">
+          <TbWorldPlus size={22} />
+        </button>
+      </Link>
+      <div className="absolute bottom-4 flex flex-col items-center gap-2">
+        {/* 타이머 */}
+        <button
+          className="hover:bg-blackAlpha-300 flex h-12 w-12 items-center justify-center rounded-lg px-2"
+          type="button">
+          <RiTimerLine size={24} />
+        </button>
+
+        {/* 환경설정 페이지 이동 */}
+        <Link href={`novel/${novelId}/`}>
+          <button
+            className="hover:bg-blackAlpha-300 flex h-12 w-12 items-center justify-center rounded-lg px-2"
+            type="button">
+            <LuSettings size={22} />
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export default NavBar;
