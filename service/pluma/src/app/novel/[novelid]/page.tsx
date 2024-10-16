fd5b19191f9bc69bdc280d9d117895948ac3ec05
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-10-16 23:05:01 +0900

    Feat: novel detail layout 생성

--- service/pluma/src/app/novel/[novelid]/page.tsx
+++ service/pluma/src/app/novel/[novelid]/page.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import NavBar from "@/components/edit/NavBar";
 import QuillEditor from "@/components/edit/QuillEditor";
 
 function NovelDetailPage() {
   return (
-    <main>
-      <header className="bg-blackAlpha-300 h-10 w-full">
-        <div />
-      </header>
-      <div className="flex h-screen w-full">
-        <NavBar />
-        <div className="bg-blackAlpha-900 h-full w-[200px]" />
-        <div className="mx-auto w-[60%]">
-          <QuillEditor />
-        </div>
-      </div>
-    </main>
+    <div className="mx-auto w-[60%]">
+      <QuillEditor />
+    </div>
   );
 }
 
