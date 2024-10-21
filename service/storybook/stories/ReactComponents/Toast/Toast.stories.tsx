0cfa1d11abeef63432f681a9819724f42d1321c7
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-10-21 22:44:20 +0900

    Feat: Toast 스토리 문서 생성

--- service/storybook/stories/ReactComponents/Toast/Toast.stories.tsx
+++ service/storybook/stories/ReactComponents/Toast/Toast.stories.tsx
@@ -1,15 +1,18 @@
 import "@byeonghyeon/react-components-button/style.css";
 import { Button } from "@byeonghyeon/react-components-button";
 import { ToastProvider, useToast } from "@byeonghyeon/react-components-toast";
+import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import "@byeonghyeon/react-components-toast/style.css";
 
-export default {
-  title: "React Components/Toast",
+const meta: Meta<typeof ToastProvider> = {
   parameters: {
     layout: "centered",
   },
 };
+
+export default meta;
+type Story = StoryObj<typeof ToastProvider>;
 
 const Example = () => {
   const { toast } = useToast();
@@ -28,7 +31,7 @@ const Example = () => {
   );
 };
 
-export const ToastStory = {
+export const ToastStory: Story = {
   render: () => (
     <ToastProvider>
       <Example />
