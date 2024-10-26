e6e0fd9fc094e25408a81688abc37249f30e41d9
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-10-26 20:45:20 +0900

    Feat: 파일 생성 API 로직 생성

--- service/pluma/src/app/api/storyFile/route.ts
+++ service/pluma/src/app/api/storyFile/route.ts
@@ -49,3 +49,43 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ message: "Error fetching files" }, { status: 500 });
   }
 }
+
+// 파일 생성
+export async function POST(req: NextRequest) {
+  try {
+    // 인증 토큰을 가져옵니다.
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    if (!token) {
+      // 인증되지 않은 사용자에 대한 처리
+      return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
+    }
+
+    // 요청 본문에서 파일 정보를 가져옵니다.
+    const { folderId, fileName } = await req.json();
+
+    // 파일 이름과 폴더 ID가 제공되지 않았을 때의 처리
+    if (!fileName || !folderId) {
+      return NextResponse.json({ message: "File name and folder ID must be provided" }, { status: 400 });
+    }
+
+    // 현재 날짜
+    const createdAt = new Date();
+
+    // 파일을 생성합니다.
+    const newFile = await prisma.storyFile.create({
+      data: {
+        name: fileName,
+        folderId: folderId,
+        content: "",
+        created_at: createdAt, // 생성 날짜// 현재 날짜를 생성 날짜로 사용합니다.
+      },
+    });
+
+    // 성공적인 응답으로 생성된 파일 정보를 반환합니다.
+    return NextResponse.json(newFile, { status: 201 });
+  } catch (error) {
+    // 오류 처리
+    console.error("Error creating file:", error);
+    return NextResponse.json({ message: "Error creating file" }, { status: 500 });
+  }
+}
