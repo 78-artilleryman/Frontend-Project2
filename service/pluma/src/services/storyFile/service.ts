3b505d73fadda6d90a4fc5576a1001a181409114
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-10-28 21:46:52 +0900

    Feat: story file list 가져오는 서비스 로직 작성

---
+++ service/pluma/src/services/storyFile/service.ts
@@ -0,0 +1,18 @@
+import { baseHttpClient } from "../baseHttpClient";
+import { CookieData } from "../novels/service";
+import { getFetchHeader } from "@/common/util/getFetchHeader";
+import { FetchStoryFileListRequest } from "@/types/storyFile/request.type";
+import { FetchStoryFileListResponse } from "@/types/storyFile/response.type";
+
+export async function GetSotryFolderList(
+  { folderId }: FetchStoryFileListRequest,
+  token?: CookieData
+): Promise<FetchStoryFileListResponse> {
+  const HEADER = getFetchHeader(token?.value, "a");
+  try {
+    return await baseHttpClient().get<FetchStoryFileListResponse>("storyFile", HEADER, { folderId });
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
---
+++ service/pluma/src/types/storyFile/response.type.ts
@@ -0,0 +1,7 @@
+export interface FetchStoryFileListResponse {
+  id: string;
+  name: string;
+  content: string;
+  folderId: string;
+  created_at: string;
+}
