777f49d3d7222bd2cbc5c9391db3cd3665ff0975
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-10-30 20:44:14 +0900

    Feat: stort mutation 로직 작성

---
+++ service/pluma/src/queries/storyFolder/mutation.ts
@@ -0,0 +1,40 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { storyFolderQueryKey } from "./queryKey";
+import { getClientSideCookie } from "@/common/util/cookieUtils";
+import { DeleteSotryFolder, PostSotryFolder } from "@/services/storyFolder/service";
+import { PostStoryFolderRequest } from "@/types/storyFolder/request.type";
+
+type mutationMethodType = "post" | "delete" | "update";
+
+export const useNovelMutation = (
+  folderId = "",
+  mutationMethod: mutationMethodType,
+  bodyData?: PostStoryFolderRequest
+) => {
+  const queryClient = useQueryClient();
+  const token = getClientSideCookie("next-auth.session-token");
+
+  return useMutation({
+    mutationFn: async () => {
+      switch (mutationMethod) {
+        case "delete":
+          return await DeleteSotryFolder({ folderId }, token);
+        case "post":
+          if (!bodyData) throw new Error("Body data is required for posting a novel");
+          return await PostSotryFolder(bodyData, token);
+        case "update":
+          return await DeleteSotryFolder({ folderId }, token);
+        default:
+          throw new Error("Invalid mutation method");
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: storyFolderQueryKey.list().queryKey,
+      });
+    },
+    onError: error => {
+      console.error("Error:", error);
+    },
+  });
+};
