8406ddd9b2076287b0672827a1e93cc4bfdb576e
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-11-01 20:09:29 +0900

    Feat: story file query문 생성

---
+++ service/pluma/src/queries/storyFile/query.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { storyFileQueryKey } from "./queryKey";
+import { GetSotryFileList } from "@/services/storyFile/service";
+import { FetchStoryFileListRequest } from "@/types/storyFile/request.type";
+import { FetchStoryFileListResponse } from "@/types/storyFile/response.type";
+
+export const useFetchStoryFileListQuery = (folderId: FetchStoryFileListRequest) => {
+  return useQuery<FetchStoryFileListResponse[] | undefined>({
+    queryKey: storyFileQueryKey.list().queryKey,
+    queryFn: () => GetSotryFileList(folderId),
+  });
+};
